10632eb0f8919cceb92f9d4d75935e9f7006310d
krzm <kmazanek@gmail.com>
2022-03-14 23:18:27 +0100

    Add AppProgSet

---
+++ CommandDotNet.Unity.Helper/AppProgSet.cs
@@ -0,0 +1,24 @@
+using CLIHelper;
+using DIHelper.Unity;
+using Unity;
+
+namespace CommandDotNet.Unity.Helper;
+
+public class AppProgSet<TAppProg>
+    : UnityDependencySet
+        where TAppProg : class
+            , IAppProgram 
+{
+    public AppProgSet(
+        IUnityContainer container) 
+        : base(container)
+    {
+    }
+
+    public override void Register()
+    {
+        Container.RegisterSingleton<IAppProgram, TAppProg>();
+        var appProg = (AppProgUnity<TAppProg>)Container.Resolve<IAppProgram>();
+        appProg.SetDIContainer(Container);
+    }
+}
